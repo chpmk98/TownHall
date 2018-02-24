e668258791de4de3d9a332f727a544e60ecf1390
chpmk98 <alvintan2020@u.northwestern.edu>
2018-02-24 13:29:25 -0600

    Add files via upload

---
+++ NUPropertyTax.js
@@ -0,0 +1,29 @@
+/*
+	This JS is for replying on the NU Property Tax Community Forum thread
+*/
+
+var num = 4;
+
+for(var i = 0; i < num; i++) {
+	document.getElementById("rb-"+i).addEventListener("click", openReply);
+	document.getElementById("sb-"+i).addEventListener("click", submitReply);
+	document.getElementById("cb-"+i).addEventListener("click", cancelReply);
+}
+
+function openReply() {
+	var index = this.id.slice(-1);
+	document.getElementById("NUProperty-r-main-"+index).style.display = "initial";
+}
+
+function submitReply() {
+	var index = this.id.slice(-1);
+	var text = document.getElementById("NUProperty-r-main-body-"+index).value;
+	document.getElementById("NUProperty-r-main-"+index).style.display = "none";
+	document.getElementById("NUProperty-nr-body-"+index).innerHTML = text;
+	document.getElementById("NUProperty-nr-"+index).style.display = "initial";
+}
+
+function cancelReply() {
+	var index = this.id.slice(-1);
+	document.getElementById("NUProperty-r-main-"+index).style.display = "none";
+}
