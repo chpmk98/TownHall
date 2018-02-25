4804b71b91e416c4e1f1655fdadf55add6c8dc14
chpmk98 <alvintan2020@u.northwestern.edu>
2018-02-25 13:38:40 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var filtering = 0;
-var filtersArr = ["Schools-filter-box", "Environment-filter-box", "Politics-filter-box", "Arts-filter-box", "Sports-filter-box"]
+var filtersArr = ["Schools-filter-box", "Environment-filter-box", "Politics-filter-box", "Arts-filter-box", "Sports-filter-box", "Other-filter-box"]
 
 document.getElementById("Schools-filter-box").onclick = function(){adjFilter("Schools-filter-box")};
 document.getElementById("Environment-filter-box").onclick = function(){adjFilter("Environment-filter-box")};
@@ -7,7 +7,47 @@ document.getElementById("Politics-filter-box").onclick = function(){adjFilter("P
 document.getElementById("Arts-filter-box").onclick = function(){adjFilter("Arts-filter-box")};
 document.getElementById("Sports-filter-box").onclick = function(){adjFilter("Sports-filter-box")};
 
-//document.getElementById("clear-filter-button").onclick = function(){clearAndEndFiltering(); filter()};
+// THIS IS NEW STUFF (2/25)!!!!! (NOTE ADDED "Other-Filter-Box" to filtersArr as well!)
+var newPosted = false;
+
+document.getElementById("Other-filter-box").onclick = function(){adjFilter("Other-filter-box")};
+
+document.getElementById("s-np").onclick = submitNewPost;
+document.getElementById("man-butt").onclick = startNewPost;
+document.getElementById("c-np").onclick = cancelNewPost;
+
+function startNewPost(){
+	document.getElementById("that-box-thing").style.display = "initial";
+}
+
+function cancelNewPost(){
+	document.getElementById("that-box-thing").style.display = "none";
+	document.getElementById("np-sub").value = "";
+	document.getElementById("np-cont").value = "";
+}
+
+function submitNewPost(){
+	var topics = document.getElementsByName("topic");
+	var top;
+	for(var i = 0; i < topics.length; i++){
+		if(topics[i].checked) {
+			top = topics[i].value;
+			break;
+		}
+	}
+	document.getElementById("New-post-cb").classList.add(top);
+
+	var subjectLine = document.getElementById("np-sub").value;
+	var content = document.getElementById("np-cont").value;
+	document.getElementById("np-subline").innerHTML = subjectLine;
+	document.getElementById("np-content").innerHTML = content;
+
+	document.getElementById("New-post-b").style.display = "initial";
+	document.getElementById("that-box-thing").style.display = "none";
+
+	newPosted = true;
+}
+// NEW STUFF (2/25) ENDS HERE for now!!!!
 
 function adjFilter(filterID){
 	if(filtering == 0) {
@@ -31,6 +71,17 @@ function startFiltering(){
 }
 
 function filter(){
+	// NEW STUFF (2/25) HERE, TOO!!!!
+	var dispNP = false;
+	var topics = document.getElementsByName("topic");
+	var top;
+	for(var i = 0; i < topics.length; i++){
+		if(topics[i].checked) {
+			top = topics[i].value;
+			break;
+		}
+	}
+	// NEW Stuff (2/25) ends here for now
 	for(i = 0; i < filtersArr.length; i++){
 		var elName = filtersArr[i];
 		console.log(elName);
@@ -41,7 +92,22 @@ function filter(){
 				document.getElementById(elName.substring(0, elName.length - 11)).style.display = "inline";
 			}
 		}
+		// also new (2/25)
+		if((top+"-box") == elName){
+			if(document.getElementById(elName).value == 1){
+				dispNP = true;
+			}
+		}
+		// end new stuff (2/25) for now
 	}
+	
+	// more new stuff (2/25)
+	if(newPosted && dispNP) {
+		document.getElementById("New-post-b").style.display = "inline";
+	} else {
+		document.getElementById("New-post-b").style.display = "none";
+	}
+	// Final end new stuff (2/25)
 }
 
 function checkIfClear(){
