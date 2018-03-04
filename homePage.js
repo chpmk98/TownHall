6b1fc60c1d58c03614296e85639f0fd966471422
Alvin Tan <alvintan2020@u.northwestern.edu>
2018-03-04 12:32:22 -0600

    changed font name for TownHall and pageTitle

--- homePage.js
+++ homePage.js
@@ -1,12 +1,16 @@
 
-var cookieMinutes = 0.5;
+var cookieMinutes = 5;
 
+// This function runs immediately upon opening the page
 (function() {
+	// This if statement hides the banner if it has already been closed
 	if(getCookie("homePagePrompt") === "dismissed") {
 		document.getElementsByClassName("signInPrompt")[0].style.display = "none";
 	}
 }) ();
 
+// Closing the banner keeps it closed for 30 seconds, even if you leave
+// the page and return.
 document.getElementById("dismissPrompt").onclick = function(){
 	document.getElementsByClassName("signInPrompt")[0].style.display = "none";
 	var time = new Date();
@@ -14,6 +18,13 @@ document.getElementById("dismissPrompt").onclick = function(){
 	document.cookie = "homePagePrompt=dismissed; expires="+time.toUTCString();
 }
 
+document.getElementById("startFilter").onclick = function(){
+	hideAllPosts();
+	
+}
+
+// Takes a cookie label and returns the value stored in the cookie
+// If there is no such cookie, returns ""
 function getCookie(cname) {
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
