4cd7aede5bd5674ff53e7819f56a1a07427f4535
maddiehong <32110309+maddiehong@users.noreply.github.com>
2018-03-04 16:44:37 -0600

    Add files via upload

---
+++ signin.js
@@ -0,0 +1,22 @@
+function login(){
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  if ( username == "mayor" && password == "Iamtheboss"){
+    dummySuccess();
+  }
+  else {
+    failLogin();
+  }
+}
+
+function dummySuccess(){
+  location = "dummyProfile.html"
+}
+
+function successLogin(){
+
+}
+
+function failLogin(){
+  alert("Incorrect Username or Password")
+}
