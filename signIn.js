06177b193b16be2c6927c438ffd09497c01adf13
maddiehong <32110309+maddiehong@users.noreply.github.com>
2018-03-04 18:01:40 -0600

    Update signIn.js

--- signIn.js
+++ signIn.js
@@ -1,7 +1,7 @@
 function login(){
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
-  if ( username == "mayor"){
+  if ( username == "mayor" && password=="evanston"){
     dummySuccess();
   }
   if ( username == "" || password == ""){}
