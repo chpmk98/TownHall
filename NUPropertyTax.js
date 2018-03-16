a8695c18f03a3e10160c4dfec0974d48909f8948
Alvin Tan <alvintan2020@u.northwestern.edu>
2018-03-15 21:43:19 -0500

    did headbar stuff on sign in

--- NUPropertyTax.js
+++ NUPropertyTax.js
@@ -15,6 +15,26 @@ inputField.addEventListener("keyup", function(event) {
 	}
 });
 
+// This function runs immediately upon opening the page
+(function() {
+	console.log("signedIn cookie value is: " + getCookie("signedIn"));
+
+	// Modifies the headbar depending on who's signed in
+	if(getCookie("signedIn") === "mayor") {
+		document.getElementById("TtYCO").style.display = "none";
+		document.getElementById("signIn").style.display = "none";
+		document.getElementById("MaCA").style.display = "initial";
+		document.getElementById("myInterests").style.display = "initial";
+		document.getElementById("mayorProfile").style.display = "initial";
+		document.getElementsByClassName("signInPrompt")[0].style.display = "none";
+	} else if(getCookie("signedIn") === "rebmeistro") {
+		document.getElementById("signIn").style.display = "none";
+		document.getElementById("myInterests").style.display = "initial";
+		document.getElementById("rebProfile").style.display = "initial";
+		document.getElementsByClassName("signInPrompt")[0].style.display = "none";
+	}
+}) ();
+
 /*
 var num = 4;
 
--- talkToYourOfficials.js
+++ talkToYourOfficials.js
@@ -1,4 +1,24 @@
 
+// This function runs immediately upon opening the page
+(function() {
+  console.log("signedIn cookie value is: " + getCookie("signedIn"));
+
+  // Modifies the headbar depending on who's signed in
+  if(getCookie("signedIn") === "mayor") {
+    document.getElementById("TtYCO").style.display = "none";
+    document.getElementById("signIn").style.display = "none";
+    document.getElementById("MaCA").style.display = "initial";
+    document.getElementById("myInterests").style.display = "initial";
+    document.getElementById("mayorProfile").style.display = "initial";
+    document.getElementsByClassName("signInPrompt")[0].style.display = "none";
+  } else if(getCookie("signedIn") === "rebmeistro") {
+    document.getElementById("signIn").style.display = "none";
+    document.getElementById("myInterests").style.display = "initial";
+    document.getElementById("rebProfile").style.display = "initial";
+    document.getElementsByClassName("signInPrompt")[0].style.display = "none";
+  }
+}) ();
+
 function submitForm() {
   var tracker = 0; //if tracker is not equal to zero, don't submit form
   if(document.getElementById("talkFirstName").value == '') {
